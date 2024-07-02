ee9f9d9831a3e10514fa26671b9c21d03fb60268
ahazimeh01 <ali.h@echovalley.io>
2024-07-02 22:20:07 +0300

    code cleanup

--- server/api.ts
+++ server/api.ts
@@ -1,32 +1,16 @@
-import {initTRPC} from "@trpc/server";
-import {createExpressMiddleware} from "@trpc/server/adapters/express";
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import cors from "cors";
 import express from "express";
-
-const t = initTRPC.create();
-
-const appRouter = t.router({
-  sayHi : t.procedure.query(() => { return "Hi"; }),
-  logToServer : t.procedure
-                    .input((v) => {
-                      if (typeof v === "string")
-                        return v;
-
-                      throw new Error("Invalid input: Expected string");
-                    })
-                    .mutation((req) => {
-                      console.log(`Client Says: ${req.input}`);
-                      return true;
-                    }),
-});
+import { appRouter } from "./routers";
 
 const app = express();
 
-app.use(cors({origin : "http://localhost:5173"}));
-app.use("/trpc", createExpressMiddleware({router : appRouter}))
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 console.log("aaaaahi1");
 
 app.listen(3000);
 
-export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
