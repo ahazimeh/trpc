dc3fd0ed6e54b70c28c6de58a31855d0ef7fdb48
ahazimeh01 <ali.h@echovalley.io>
2024-07-02 23:42:35 +0300

    procedure

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,17 +1,23 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import { AppRouter } from "../../server/api"
+import { AppRouter } from "../../server/api";
 
 const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({
-    url:"http://localhost:3000/trpc"
-  })]
-})
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000/trpc",
+    }),
+  ],
+});
 
 async function main() {
   // const result = client.sayHi.query();
   // console.log(result)
   // const result = await client.logToServer.mutate("Hi from client")
-   const result = await client.users.getUser.query()
-  console.log(result)
+  // const result = await client.users.get.query({ userId: "1234" });
+  const result = await client.users.update.mutate({
+    userId: "23432",
+    name: "Kyle",
+  });
+  console.log(result);
 }
-main()
+main();
--- server/routers/users.ts
+++ server/routers/users.ts
@@ -1,7 +1,19 @@
+import { z } from "zod";
 import { t } from "../trpc";
 
+const userProcedure = t.procedure.input(z.object({ userId: z.string() }));
+
 export const userRouter = t.router({
-  getUser: t.procedure.query(() => {
-    return { id: 1, name: "Kyle" };
+  get: userProcedure.query(({ input }) => {
+    return { id: input.userId };
   }),
+  update: userProcedure
+    .input(z.object({ name: z.string() }))
+    .output(z.object({ name: z.string(), id: z.string() }))
+    .mutation((req) => {
+      console.log(
+        `updating user ${req.input.userId} to have the name ${req.input.name}`,
+      );
+      return { id: req.input.userId, name: req.input.name,  };
+    }),
 });
